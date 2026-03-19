e651ad85530379daa24c184e1bfce4bcf3d3f434
fvsgamers <fvsgamers@gmail.com>
2026-03-19 11:56:47 -0300

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -1,5 +1,7 @@
 module.exports = (client) => {
   client.once('ready', () => {
     console.log(`✅ Bot online como ${client.user.tag}`);
+    botOnline = true; // ⚠️ variável do index.js
+    checkReady();
   });
 };
